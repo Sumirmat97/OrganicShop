7601a3b8d2c03f92de02f479953a734c31e3e30b
Sumirmat97 <sumir.mathur8@gmail.com>
2020-11-02 12:47:58 +0530

    CSS changes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,6 @@
 import { SharedModule } from './../shared/shared.module';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { CustomFormsModule } from 'ngx-custom-validators';
 
 import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
 import { HomeComponent } from './components/home/home.component';
